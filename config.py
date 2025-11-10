3dc2bb66778fcf5a5ac6b5ccf2ab3732db0df7e2
16ezymets <alesha.fomin2007@gmail.com>
2025-11-10 13:49:14 +0300

    configs created

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", '')
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+
+DATABASE_URL = os.getenv("DATABASE_URL", 'sqlite:///telegram_monitor.db')
+
+DEFAULT_MIN_REACTIONS = 100
