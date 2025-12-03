30708c4da40d609b745558c6b14d36cb93c990fa
16ezymets <alesha.fomin2007@gmail.com>
2025-12-03 14:28:25 +0300

    Added relationships

--- sql_database.py
+++ sql_database.py
@@ -1,54 +1,46 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, Text
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import  create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from config import DATABASE_URL
 
 Base = declarative_base()
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class User(Base):
-    """
-    Table 'users' : (id, telegram_id, username, min_reactions)
-
-    Columns types:
-    id: int, telegram_id: int, username: string, min_reactions: int
-    """
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(Integer, unique=True, index=True)
-    username = Column(String, nullable=True) # Ex: @username
+    username = Column(String, nullable=True)
     min_reactions = Column(Integer, default=100)
+
+    # Только один relationship
+    channels = relationship(
+        "Channel",
+        secondary="user_channels",
+        backref="users"
+    )
 
 
 class Channel(Base):
-    """
-    Table 'channel' : (id, channel_username, channel_title, is_active)
+    __tablename__ = "channels"
 
-    Columns types:
-    id: int, channel_username: str, channel_title: string, is_active: bool
-    """
-    __tablename__ = "channel"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    channel_username = Column(String, unique=True, index=True) # Ex: @channel
+    id = Column(Integer, primary_key=True, index=True)
+    channel_username = Column(String, unique=True, index=True)
     channel_title = Column(String)
     is_active = Column(Boolean, default=True)
 
 
+
 class UserChannel(Base):
-    """
-    Table 'channels' : (id, user_id, channel_id)
+    __tablename__ = "user_channels"
 
-    Columns types:
-    id: int, user_id: int, channel: int
-    """
-    __tablename__ = "channels"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, index=True)
-    chanel_id = Column(Integer, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.telegram_id'), index=True)
+    channel_id = Column(Integer, ForeignKey('channels.id'), index=True)
 
 
 class SentPost(Base):
@@ -64,24 +56,22 @@ class SentPost(Base):
     user_id = Column(Integer, index=True)
     channel_id = Column(Integer, index=True)
     post_id = Column(Integer, index=True)
-    sent_at = Column(Integer) # timestamp
+    sent_at = Column(Integer)
 
-    @staticmethod
-    def create_tables():
-        """
-        Creates all tables
-        :return: None
-        """
-        Base.metadata.create_all(bind=engine)
 
-    @staticmethod
-    def get_db():
-        """
-        Gets database if possible
-        :return: Generator[Session, Any, None] | None
-        """
-        db = SessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
+
+def create_tables():
+    """
+    Creates all tables
+    :return: None
+    """
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    """
+    Creates and get database
+    :return: Generator[Session, Any, None] | None
+    """
+    db = SessionLocal()
+    return db
